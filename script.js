46a512f15e3318200926dc3bbfa42777d8ba4fa5
Majesty <63979930+CollinBrennan@users.noreply.github.com>
2020-05-11 15:35:12 -0400

    Add files via upload
    -Revamped character weights
    -Fixed input bugs

--- script.js
+++ script.js
@@ -5,13 +5,16 @@
 // -night mode
 // -add seperate IPA chart for mobile, dont add table head just make it a group of characters
 // -add titles to IPA symbols
+// -made comments italicized
 
-// To Do:
+// To Add:
 // -add LITERAL notation ("C")V(N), C"V"N
 // -make phonetic groups possible in replacemenets and filters (VV: V)
+// -floating point weights (fix the equation in general)
+// -favicon
 
 // To Fix:
-//
+// 
 
 function generate(
   _min,
@@ -21,7 +24,9 @@ function generate(
   _filters,
   _rewrites,
   _characters,
-  _duplicates
+  _duplicates,
+  _charWeights,
+  _writeWeights
 ) {
 // Variables
   const min = _min
@@ -32,7 +37,12 @@ function generate(
   const rewrites = new Map(_rewrites)
   const characters = new Map(_characters)
   const duplicates = _duplicates
+
+  const charWeights = new Map(_charWeights)
+  const writeWeights = new Map(_writeWeights)
+
   let lexicon = []
+
 // Functions
   function unique(list) {
     return Array.from(new Set(list))
@@ -40,6 +50,24 @@ function generate(
 
   function random(min, max) {
     return Math.floor(Math.random() * (max - min)) + min
+  }
+
+  function weightedRandom(items, weights) {
+
+    function float(min, max) {
+      return Math.random() * (max - min) + min
+    }
+
+    const sum = weights.reduce((a, b) => a + b, 0)
+    let ran = float(0, sum)
+    for (let i = 0; i < items.length; i++) {
+      if (ran < weights[i]) {
+        return [items[i]]
+      }
+      else {
+        ran -= weights[i]
+      }
+    }
   }
 // Loop
   while (lexicon.length < count) {
@@ -58,8 +86,9 @@ function generate(
   // Make word from template
     let word = ''
     for (let key of template) {
-      let values = characters.get(key)
-      word += values && values.length > 0 ? values[random(0, values.length)] : key
+      let letters = characters.get(key)
+      let weights = charWeights.get(key)
+      word += letters && letters.length > 0 ? weightedRandom(letters, weights) : key
     }
   // Randomly keep or delete letters in parentheses
     const pRegex = /\(([^\(]*?)\)/
@@ -68,9 +97,11 @@ function generate(
     }
   // Rewrites
     for (let key of rewrites.keys()) {
-      let values = rewrites.get(key)
-      while (word.includes(key))
-        word = word.replace(key, values.length > 0 ? values[random(0, values.length)] : '')
+      let letters = rewrites.get(key)
+      let weights = writeWeights.get(key)
+      while (word.includes(key)) {
+        word = word.replace(key, letters.length > 0 ? weightedRandom(letters, weights) : '')
+      }
     }
   // Filters
     let push = true
@@ -90,7 +121,9 @@ function generate(
     }
 // Return
   // console.log('Characters:', characters, '\n')
+  // console.log('Character Weights:', charWeights, '\n')
   // console.log('Rewrites:', rewrites, '\n')
+  // console.log('Rewrite Weights:', writeWeights, '\n')
   // console.log('Filters:', filters, '\n')
   // console.log('Patterns:', patterns, '\n')
   return lexicon
@@ -101,6 +134,9 @@ function takeInput() {
 // Functions
   function format(element, regex) {
     const array = []
+  // Remove comments
+    element = element.replace(/\/\/[^,\n]*/g, '')
+    element = element.replace(/\/[^\s,]*/g, '')
   // Create the arrays
     const matches = element.match(regex)
     if (matches !== null) {
@@ -108,32 +144,45 @@ function takeInput() {
         let items = []
         match = match.replace(/\(|\)|\[|\]|"|:/g, ' ')
         match = match.match(/\S+/g)
-        // Add weights
-        const wRegex = /(\S+)\*(\d+)/
         let name = match[0]
         let contents = match.slice(1, match.length)
-        let weightedContents = []
-        for (item of contents) {
-          if (wRegex.test(item)) {
-            let groups = wRegex.exec(item)
-            let counter = groups[2] > 100 ? 100 : groups[2]
-            for (let i = 0; i < counter; i++) {
-              weightedContents.push(groups[1])
-            }
-          } else {
-            weightedContents.push(item)
-          }
-        }
-        array.push([name, weightedContents])
+        array.push([name, contents])
       }
     }
     return array
   }
 
   function listFormat(element) {
+  // Remove comments
+    element = element.replace(/\/\/[^,\n]*/g, '')
+    element = element.replace(/\/[^\s,]*/g, '')
+  // Split list
     const regex = /[^\s,][^,]*[^\s,]|[^\s,]/g
     return element.match(regex)
   }
+
+  function makeWeights(input) {
+    const array = []
+    const regex = /(.+)\*((\d+\.*\d*)|(\.\d+))/
+    for (item of input) {
+      let pair = []
+      let weights = []
+      let contents = item[1]
+      pair.push(item[0])
+      for (i in contents) {
+        if (regex.test(contents[i])) {
+          weights.push(Number(regex.exec(contents[i])[2]))
+          item[1][i] = regex.exec(contents[i])[1]
+        } else {
+          weights.push(1)
+        }
+      }
+      pair.push(weights)
+      array.push(pair)
+    }
+    return array
+  }
+
 // Variables
   const myMin = Number(document.getElementById("min").value)
   const myMax = Number(document.getElementById("max").value)
@@ -143,6 +192,9 @@ function takeInput() {
   const myRewrites = format(document.getElementById("rewrites").value, /[^\s,]+\s*:[^,]*/g)
   const myCharacters = format(document.getElementById("characters").value, /[^\s,]\s*:[^,]*/g)
   const myDuplicate = document.getElementById("duplicates").checked
+
+  const myCharWeights = makeWeights(myCharacters)
+  const myWriteWeights = makeWeights(myRewrites)
 
   const newLine = document.getElementById("new").checked
 
@@ -154,7 +206,9 @@ function takeInput() {
     myFilters,
     myRewrites,
     myCharacters,
-    myDuplicate
+    myDuplicate,
+    myCharWeights,
+    myWriteWeights
   )
 // Fix form inputs
   myMax < myMin ? document.getElementById("max").value = myMin : null
@@ -204,9 +258,18 @@ function showIPA() {
 const letters = document.getElementsByClassName("letter")
 for (let letter of letters) {
   letter.addEventListener("mousedown", () => {
-    let active = document.activeElement.id
-    if (active === "characters" || active === "rewrites" || active === "patterns" || active === "filters") {
-      document.activeElement.value += letter.innerHTML
+    let active = document.activeElement
+    if (active.id === "characters" || active.id === "rewrites" || active.id === "patterns" || active.id === "filters") {
+      let start = active.selectionStart;
+      let end = active.selectionEnd;
+      if (active.selectionStart || active.selectionStart === '0') {
+        active.value = active.value.substring(0, start)
+            + letter.innerHTML
+            + active.value.substring(end, active.value.length);
+      } else {
+          myField.value += myValue;
+      }
+      active.setSelectionRange(start + 1, start + 1)
     }
     event.preventDefault()
   })
@@ -215,9 +278,18 @@ for (let letter of letters) {
 const puncts = document.getElementsByClassName("punct")
 for (let punct of puncts) {
   punct.addEventListener("mousedown", () => {
-    let active = document.activeElement.id
-    if (active === "characters" || active === "rewrites" || active === "patterns" || active === "filters") {
-      document.activeElement.value += punct.getAttribute("value")
+    let active = document.activeElement
+    if (active.id === "characters" || active.id === "rewrites" || active.id === "patterns" || active.id === "filters") {
+      let start = active.selectionStart;
+      let end = active.selectionEnd;
+      if (active.selectionStart || active.selectionStart === '0') {
+        active.value = active.value.substring(0, start)
+            + punct.getAttribute("value")
+            + active.value.substring(end, active.value.length);
+      } else {
+          myField.value += myValue;
+      }
+      active.setSelectionRange(start + 1, start + 1)
     }
     event.preventDefault()
   })
@@ -227,19 +299,23 @@ for (let punct of puncts) {
 if (!onMobile) {
   function resetInputs () {
     document.getElementById("characters").style.position = "initial"
+    document.getElementById("characters").style.minWidth = "initial"
     document.getElementById("characters").style.left = "initial"
     document.getElementById("characters").style.top = "initial"
 
     document.getElementById("rewrites").style.position = "initial"
+    document.getElementById("rewrites").style.minWidth = "initial"
     document.getElementById("rewrites").style.left = "initial"
     document.getElementById("rewrites").style.top = "initial"
     
     document.getElementById("patterns").style.position = "initial"
+    document.getElementById("patterns").style.minWidth = "initial"
     document.getElementById("patterns").style.maxWidth = "initial"
     document.getElementById("patterns").style.left = "initial"
     document.getElementById("patterns").style.top = "initial"
     
     document.getElementById("filters").style.position = "initial"
+    document.getElementById("filters").style.minWidth = "initial"
     document.getElementById("filters").style.maxWidth = "initial"
     document.getElementById("filters").style.left = "initial"
     document.getElementById("filters").style.top = "initial"
@@ -259,6 +335,7 @@ if (!onMobile) {
           let calculate = window.innerWidth/2 - (active.id === "rewrites" ? active.offsetWidth : active.innerWidth)/2
           active.style.left = calculate + 'px'
           active.style.top = "16px"
+          active.style.minWidth = "480px"
           active.style.maxWidth = "480px"
           active.style.position = "fixed"
         } else {
